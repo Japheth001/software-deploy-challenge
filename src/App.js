50f8d9371b98e5fdc402394ee8677483b1d7163b
redesigned-happiness <mumokimeu3@gmail.com>
2020-12-17 05:21:27 +0300

    deploy my react to my github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import ChuckPic from './img/chuckpic.jpg';
 
 function App() {
+
+ const [jokes, setJokes]=useState([])
+ const [query, setQuery]=useState('teeth')
+
+ useEffect(()=>{
+   getResults();
+ })
+
+ const getResults=async()=>{
+   const response=await axios
+   .get(`https://api.chucknorris.io/jokes/search?query=${query}`)
+   setJokes(response.data.result)
+ }
+
+
+ const handleSearch = (e)=>{
+   e.preventDefault()
+   getResults();
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+              <h className="title">Chuck Norris API</h>
+              <img src={ChuckPic} alt="Chuck Norris"/>
+              <h5 className="initials">Designed by Japheth : Email: mumokimeu3@gmail.com</h5>
+          </div>
+
+        </div>
+        <h1 className="category">Enter Word of your choice</h1>
+      
+      </div>
+      
+      <form onSubmit={handleSearch}>
+
+          <input type="text"
+            onChange={e=>setQuery(e.target.value)}
+            value={query}
+          />
+      <button type="submit">Search</button>
+      </form>
+
+
+      {jokes.map(joke=>{
+        return(
+          <p>{joke.value}</p>
+        )
+      })}
     </div>
+    
   );
 }
 
